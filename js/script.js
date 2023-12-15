bee9ac183b6791a642fe84b5c8048b4887ab8a31
MarcoFonti <theemporiumg@gmail.com>
2023-12-15 16:46:20 +0100

    add variable nome

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 // Check
 console.log('JS OK')
+
+/* 
+Dichairo Variabile 
+Chiedo il nome al''utente
+Salvo in una variabile 
+*/
+const firstName = window.prompt ('Come ti chiami?');
+console.log(firstName);
---
+++ textPassages.md
@@ -0,0 +1,5 @@
+1 Diachiaro variabile: nome
+
+2 Chiedo all'utente il suo nome
+
+3 Salvo in una variabile
